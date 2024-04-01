80bff2689c6b21a385d4edc659762635ee3dc0b3
Thomas Krause <thomaskrause@posteo.de>
2024-04-01 19:06:05 +0200

    Add test that check that the label is truncated

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,9 +1,12 @@
+use egui::{Label, Widget};
+
 #[derive(Default)]
 struct HelloApp {
-    counter: u32,
+    counter: u128,
 }
 
 impl HelloApp {
+    /// Render the context without access to the eframe::Frame.
     fn render(&mut self, ctx: &egui::Context) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Hello World");
@@ -11,7 +14,7 @@ impl HelloApp {
                 self.counter += 1;
             };
             ui.separator();
-            ui.label(format!("Clicked {} times", self.counter));
+            Label::new(format!("Clicked {} times", self.counter)).truncate(true).ui(ui);
             ui.separator();
         });
     }
@@ -19,6 +22,9 @@ impl HelloApp {
 
 impl eframe::App for HelloApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // We do the actual rendering in a separate function that does not take
+        // the eframe::Frame as argument to make it easier to use it in the
+        // screenshot tests.
         self.render(ctx);
     }
 }
@@ -42,13 +48,39 @@ mod tests {
     use crate::HelloApp;
     use egui_screenshot_testing::TestBackend;
 
+    /// Simple test that renders the application with the test backend and
+    /// compares the screenshot with the stored one.
     #[test]
     fn test_initial_screen() {
+        // Create you application like normal.
         let mut app = HelloApp::default();
+
+        // Define the backend with two directories where all the screenshot files are located.
         let mut backend = TestBackend::new("examples/expected", "examples/actual", |_ctx| {});
 
         backend.assert_screenshot_after_n_frames(
-            "initial_hello_world.png",
+            "hello_world_initial.png",
+            (150, 100),
+            5,
+            move |ctx| {
+                app.render(ctx);
+            },
+        );
+    }
+
+    /// This crate does not allow to emulate any input to egui, but it can
+    /// modify the application state and check that the result renders
+    /// correctly and that the number is truncated.
+    #[test]
+    fn test_large_number() {
+        let mut app = HelloApp::default();
+        // Use a very large number and check that the rendering still works.
+        app.counter = 100_000_000_000_000;
+
+        let mut backend = TestBackend::new("examples/expected", "examples/actual", |_ctx| {});
+
+        backend.assert_screenshot_after_n_frames(
+            "hello_world_large_number.png",
             (150, 100),
             5,
             move |ctx| {
